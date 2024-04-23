75837dc3f94e14c4f0c706d6dabfb5b82a5be71d
crazy-samuray <ayaz33erdogan@gmail.com>
2024-04-23 21:11:54 +0300

    Update shooter2.py

--- shooter2.py
+++ shooter2.py
@@ -1,40 +1,39 @@
 from pygame import *
-import pygame
+import pygame.sprite
 from random import randint
-from time import time as timer #yeni
+from time import time as timer
 
 font.init()
 font1 = font.Font(None, 80)
 win = font1.render('YOU WIN!', True, (255, 255, 255))
 lose = font1.render('YOU LOSE!', True, (180, 0, 0))
- 
+
 font2 = font.Font(None, 36)
- 
 
 mixer.init()
 mixer.music.load('C://Users//Monster1//Documents//GitHub//shooter_oyun//space.ogg')
 mixer.music.play()
 fire_sound = mixer.Sound('C://Users//Monster1//Documents//GitHub//shooter_oyun//fire.ogg')
- 
-img_back = "C://Users//Monster1//Documents//GitHub//shooter_oyun//galaxy.jpg" #oyunun arka planı
-img_bullet = "C://Users//Monster1//Documents//GitHub//shooter_oyun//bullet.png" #mermi
-img_hero = "C://Users//Monster1//Documents//GitHub//shooter_oyun//rocket.png" #kahraman
-img_enemy = "C://Users//Monster1//Documents//GitHub//shooter_oyun//ufo.png" #düşman
-img_ast = "C://Users//Monster1//Documents//GitHub//shooter_oyun//asteroid.png" #düşman #yeni
 
-score = 0 #düşmüş gemiler
-goal = 10 #Kazanmak için kaç gemiyi vurman gerekiyor
-lost = 0 #kaçırılan gemiler
-max_lost = 3 #Bu kadar çok şeyi kaçırırsanız kaybettiniz.
-life = 3 #can sayısı
+img_back = "C://Users//Monster1//Documents//GitHub//shooter_oyun//galaxy.jpg" 
+img_bullet = "C://Users//Monster1//Documents//GitHub//shooter_oyun//bullet.png" 
+img_hero = "C://Users//Monster1//Documents//GitHub//shooter_oyun//rocket.png" 
+img_enemy = "C://Users//Monster1//Documents//GitHub//shooter_oyun//ufo.png" 
+img_ast = "C://Users//Monster1//Documents//GitHub//shooter_oyun//asteroid.png" 
+
+score = 0
+goal = 10
+lost = 0
+max_lost = 3
+life = 3
 
 class GameSprite(sprite.Sprite):
    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
        sprite.Sprite.__init__(self)
- 
+
        self.image = transform.scale(image.load(player_image), (size_x, size_y))
        self.speed = player_speed
- 
+
        self.rect = self.image.get_rect()
        self.rect.x = player_x
        self.rect.y = player_y
@@ -51,9 +50,13 @@ class Player(GameSprite):
            self.rect.x += self.speed
 
    def fire(self):
+       bullet = Bullet(img_bullet, self.rect.centerx, self.rect.top, 15, 20, -7)  # Mermi hızını değiştirin
+       bullets.add(bullet)
+
+   def fire(self):
        bullet = Bullet(img_bullet, self.rect.centerx, self.rect.top, 15, 20, -15)
        bullets.add(bullet)
- 
+
 class Enemy(GameSprite):
     def update(self):
         self.rect.y += self.speed
@@ -67,7 +70,7 @@ class Enemy(GameSprite):
 class Bullet(GameSprite):
    def update(self):
        self.rect.y += self.speed
-     
+
        if self.rect.y < 0:
            self.kill()
 
@@ -80,13 +83,11 @@ background = transform.scale(image.load(img_back), (win_width, win_height))
 ship = Player(img_hero, 5, win_height - 100, 80, 100, 10)
 
 monsters = sprite.Group()
-
 for i in range(1, 6):
    monster = Enemy(img_enemy, randint(80, win_width - 80), -40, 80, 50, randint(1, 5))
    monsters.add(monster)
 
 asteroids = sprite.Group()
-
 for i in range(1, 3):
     asteroid = Enemy(img_ast, randint(30, win_width - 30), -40, 80, 50, randint(1,7))
     asteroids.add(asteroid)
@@ -94,51 +95,47 @@ for i in range(1, 3):
 bullets = sprite.Group()
 
 finish = False
-
 run = True
-
 rel_time = False
 num_fire = 0
 
 while run:
-
     for e in event.get():
         if e.type == QUIT:
             run = False
 
         elif e.type == KEYDOWN:
             if e.key == K_SPACE:
-                if num_fire < 5 and rel_time == False:
+                if num_fire < 5 and not rel_time:
                     num_fire = num_fire +1
                     fire_sound.play()
                     ship.fire()
 
-                if num_fire >= 5 and rel_time == False:
+                if num_fire >= 5 and not rel_time:
                     last_time = timer()
                     rel_time = True
- 
+
     if not finish:
         window.blit(background,(0,0))
         ship.update()
         monsters.update()
         asteroids.update()
         bullets.update()
- 
+
         ship.reset()
         monsters.draw(window)
         asteroids.draw(window)
         bullets.draw(window)
 
-        if rel_time == True:
+        if rel_time:
             now_time = timer()
-
             if now_time - last_time < 3:
                 reload = font2.render('wait, reload....', 1, (150, 0, 0))
                 window.blit(reload, (260, 460))
             else:
                 num_fire = 0
                 rel_time = False
-        
+
         collides = sprite.groupcollide(monsters, bullets, True, True)
         for c in collides:
             score = score + 1
@@ -147,21 +144,20 @@ while run:
 
         if sprite.spritecollide(ship, monsters, False) or sprite.spritecollide(ship, asteroids, False):
             sprite.spritecollide(ship, monsters, True)
-            sprite.spritecollide(ship, asteroid, True)
-            life = life +1
+            sprite.spritecollide(ship, asteroids, True)
+            life = life - 1
 
-        if life == 0 or lost >= max_lost:
+        if life <= 0 or lost >= max_lost:
             finish = True
             window.blit(lose, (200, 200))
- 
+
         if score >= goal:
             finish = True
             window.blit(win, (200, 200))
- 
 
         text = font2.render("Sayı: " + str(score), 1, (255, 255, 255))
         window.blit(text, (10, 20))
- 
+
         text_lose = font2.render("Cevapsız "+ str(lost), 1, (255, 255, 255))
         window.blit(text_lose, (10, 50))
 
@@ -169,13 +165,12 @@ while run:
             life_color = (0, 150, 0)
         if life == 2:
             life_color = (150, 150, 0)
-        if life ==1:
+        if life == 1:
             life_color = (150, 0, 0)
-        
+
         text_life = font1.render(str(life), 1, life_color)
         window.blit(text_life, (650, 10))
 
- 
         display.update()
 
     else:
@@ -190,15 +185,14 @@ while run:
             m.kill()
         for a in asteroids:
             a.kill()
- 
+
         time.delay(3000)
         for i in range(1, 6):
             monster = Enemy(img_enemy, randint(80, win_width - 80), -40, 80, 50, randint(1, 5))
             monsters.add(monster)
-        
+
         for i in range(1, 3):
             asteroid = Enemy(img_ast, randint(30, win_width - 30), -40, 80, 50, randint(1, 7))
             asteroids.add(asteroid)
- 
-pygame.time.delay(50)
 
+    pygame.time.delay(350)
